3625217aa02372ee7cc316e8d0f4295d04f58208
nubroc <numa.brocandel@efrei.net>
2025-01-17 17:37:54 +0100

    create welcome page controller & welcome vue

---
+++ src/Controller/WelcomepageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class WelcomepageController extends AbstractController
+{
+    #[Route('/', name: 'welcomepage')]
+    public function index(): Response
+    {
+        return $this->render('welcomepage/index.html.twig', [
+            'controller_name' => 'WelcomepageController',
+        ]);
+    }
+}
